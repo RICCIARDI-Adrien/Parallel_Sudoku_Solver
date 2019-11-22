81d8247dbb80a8161b6560f9b2806a7e4f3ca2f8
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2019-11-22 23:18:12 +0100

    Increased maximum simultaneous worker threads count.

--- Includes/Configuration.h
+++ Includes/Configuration.h
@@ -17,7 +17,7 @@
 /** Width and height of a grid in cells (a grid is made of CONFIGURATION_GRID_MAXIMUM_SIZE x CONFIGURATION_GRID_MAXIMUM_SIZE cells). */
 #define CONFIGURATION_GRID_MAXIMUM_SIZE 16
 
-/** The maximum amount of threads that allowed to run simultaneously. */
-#define CONFIGURATION_WORKERS_MAXIMUM_COUNT 128
+/** The maximum amount of threads that are allowed to run simultaneously. */
+#define CONFIGURATION_WORKERS_MAXIMUM_COUNT 1024
 
 #endif
