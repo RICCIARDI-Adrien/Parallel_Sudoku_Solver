71b7817c77fa12cd624e0688b4fac2541d7e2352
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2020-01-12 18:16:27 +0100

    Refactored Cells Stack module to use new logging system.

--- Sources/Cells_Stack.c
+++ Sources/Cells_Stack.c
@@ -4,13 +4,18 @@
  */
 #include <assert.h>
 #include <Cells_Stack.h>
-#include <Configuration.h>
-#include <stdio.h>
+#include <Log.h>
+
+//-------------------------------------------------------------------------------------------------
+// Private constants
+//-------------------------------------------------------------------------------------------------
+/** Enable or disable this module debug messages. */
+#define CELLS_STACK_IS_DEBUG_ENABLED 0
 
 //-------------------------------------------------------------------------------------------------
 // Private functions
 //-------------------------------------------------------------------------------------------------
-#if CONFIGURATION_IS_DEBUG_ENABLED
+#if CELLS_STACK_IS_DEBUG_ENABLED
 	/** Display a stack content.
 	 * @param Pointer_Stack The stack to display content.
 	 */
@@ -18,8 +23,8 @@
 	{
 		int i;
 		
-		printf("Count = %d\n", Pointer_Stack->Stack_Index);
-		for (i = Pointer_Stack->Stack_Index - 1; i >= 0; i--) printf("Row = %d, Column = %d\n", Pointer_Stack->Cells_Coordinates[i].Row, Pointer_Stack->Cells_Coordinates[i].Column);
+		LOG(CELLS_STACK_IS_DEBUG_ENABLED, "Count = %d\n", Pointer_Stack->Stack_Index);
+		for (i = Pointer_Stack->Stack_Index - 1; i >= 0; i--) LOG(CELLS_STACK_IS_DEBUG_ENABLED, "Row = %d, Column = %d\n", Pointer_Stack->Cells_Coordinates[i].Row, Pointer_Stack->Cells_Coordinates[i].Column);
 	}
 #endif
 
@@ -44,8 +49,7 @@ int CellsStackPush(TCellsStack *Pointer_Stack, int Cell_Row, int Cell_Column)
 	Pointer_Cell_Coordinates->Column = Cell_Column;
 	Pointer_Stack->Stack_Index++;
 	
-	#if CONFIGURATION_IS_DEBUG_ENABLED
-		printf("[%s] ", __FUNCTION__);
+	#if CELLS_STACK_IS_DEBUG_ENABLED
 		CellsStackShow(Pointer_Stack);
 	#endif
 	return 1;
@@ -63,8 +67,7 @@ int CellsStackReadTop(TCellsStack *Pointer_Stack, int *Pointer_Cell_Row, int *Po
 	*Pointer_Cell_Row = Pointer_Cell_Coordinates->Row;
 	*Pointer_Cell_Column = Pointer_Cell_Coordinates->Column;
 	
-	#if CONFIGURATION_IS_DEBUG_ENABLED
-		printf("[%s] ", __FUNCTION__);
+	#if CELLS_STACK_IS_DEBUG_ENABLED
 		CellsStackShow(Pointer_Stack);
 	#endif
 	return 1;
