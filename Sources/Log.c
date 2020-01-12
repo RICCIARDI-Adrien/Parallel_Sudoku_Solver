c23b7a32a58bf9d9221ecd535aefa3af890cf1e5
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2020-01-12 18:15:43 +0100

    Made logs messages thread safe.

--- Sources/Log.c
+++ Sources/Log.c
@@ -8,13 +8,23 @@
 #include <stdio.h>
 
 //-------------------------------------------------------------------------------------------------
+// Private variables
+//-------------------------------------------------------------------------------------------------
+static pthread_mutex_t Logs_Mutex = PTHREAD_MUTEX_INITIALIZER;
+
+//-------------------------------------------------------------------------------------------------
 // Public functions
 //-------------------------------------------------------------------------------------------------
 void LogPrintMessage(const char *Pointer_String_Format, ...)
 {
 	va_list Arguments_List;
 	
+	pthread_mutex_lock(&Logs_Mutex);
+	
+	// Display log message
 	va_start(Arguments_List, Pointer_String_Format);
 	vprintf(Pointer_String_Format, Arguments_List);
 	va_end(Arguments_List);
+	
+	pthread_mutex_unlock(&Logs_Mutex);
 }
